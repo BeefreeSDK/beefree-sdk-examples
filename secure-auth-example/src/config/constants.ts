176eca1cc0e4db385f0b5601539c407fbfc03d7b
Roberto Marchetti <marchetti@beefree.io>
2025-08-28 14:47:56 +0200

    feat: fix of AUTH_API_URL

--- secure-auth-example/src/config/constants.ts
+++ secure-auth-example/src/config/constants.ts
@@ -1,4 +1,4 @@
-export const AUTH_API_URL = '/auth/token'
+export const AUTH_API_URL = import.meta.env.VITE_BEEFREE_AUTH_PROXY_URL || '/auth/token'
 
 export const BEEFREE_CONFIG = {
   container: 'bee-plugin-container',
