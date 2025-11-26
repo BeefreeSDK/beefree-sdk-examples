cf118ee101a28cdcef509eee4e1a2c2cc9a2e1af
Alexandre Madurell de Otero <alexandre.madurell@beefree.io>
2025-11-26 18:06:35 +0100

    chore: render template export pdf truly stand-alone

--- template-export-pdf-example/vite.config.ts
+++ template-export-pdf-example/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         changeOrigin: true
       },
       '/auth': {
-        target: 'http://localhost:3000',
+        target: `http://localhost:${process.env.PORT || 3001}`,
         changeOrigin: true,
         secure: false
       }
