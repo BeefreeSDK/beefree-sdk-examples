7b9a1d6b97d55a8b1b10a2cd16e7aa8314536c80
Alexandre Madurell de Otero <alexandre.madurell@beefree.io>
2025-11-26 18:12:58 +0100

    chore: remove unnecessary env.ts file

--- template-export-pdf-example/src/env.ts
+++
@@ -1,5 +0,0 @@
-export const envs = {
-  AUTH_PROXY_URL: import.meta.env.VITE_BEEFREE_AUTH_PROXY_URL || '/auth/token',
-  TEMPLATE_URL: import.meta.env.VITE_BEEFREE_TEMPLATE_URL || 'https://rsrc.getbee.io/api/templates/m-bee',
-};
-
